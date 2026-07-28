2bae04e8dbc4a80050e049e8ea4061e9d72065d4
MysticalHeat <ilyaelmanov@gmail.com>
2026-07-29 03:43:06 +0900

    feat(settings): add dotenv support via django-environ
    - Add .env and .vscode to .gitignore.
    - Import django-environ and load .env in settings.
    - Replace hard‑coded SECRET_KEY and DEBUG with env variables.
    - Register app.apps.AppConfig in INSTALLED_APPS.
    - Add django-environ dependency to pyproject.toml.

--- config/settings.py
+++ config/settings.py
@@ -11,19 +11,24 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env(
+    DJANGO_DEBUG=(bool, False)
+)
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z2es92l^6*1drc&*7@_-670qw9&$p9sr&mpktgu=u8t)img_4v'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DJANGO_DEBUG")
+
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
